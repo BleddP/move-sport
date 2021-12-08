e2bf15ae4993f3fddc762cf651400bbeb646fa5a
Bleddyn Pijpers <bleddyn.pijpers@foursites.nl>
2021-12-08 17:31:14 +0800

    updated controller

--- src/api/homepage/controllers/homepage.js
+++ src/api/homepage/controllers/homepage.js
@@ -8,12 +8,17 @@ const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::homepage.homepage', ({ strapi }) => ({
     async find(ctx) {
-        // const populate = [
-        //     'Header',
-        //     'Header.image'
-        // ]
-        // const content = await strapi.services['api::homepage.homepage'].find(ctx.query, populate)
-        // return content
+        const populateList = [
+            'Header.Header_image',
+            'Header.Buttons',
+            'Clients.clients',
+            'About',
+            'About.Image'
+        ]
+        // Push any additional query params to the array
+        populateList.push(ctx.query.populate)
+        ctx.query.populate = populateList.join(',')
+
         const content = await super.find(ctx)
         return content
     }
