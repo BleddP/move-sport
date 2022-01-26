a3f2e7e81578f3328218dd21a02439a1edbbf50b
Bleddyn Pijpers <bleddyn.pijpers@cloud-studio.co.uk>
2022-01-26 22:25:13 +0100

    strapi cloudinary bug fix

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,10 +1,13 @@
 module.exports = ({ env }) => ({
     upload: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env(process.env.CLOUD_NAME),
-        api_key: env(process.env.CLOUD_API_KEY),
-        api_secret: env(process.env.CLOUD_SECRET),
+      config: {
+        provider: "cloudinary",
+        providerOptions: {
+          cloud_name: env("CLOUD_NAME"),
+          api_key: env("CLOUD_API_KEY"),
+          api_secret: env("CLOUD_SECRET"),
+        },
       },
-    },    
+    },
   });
+  
--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,12 @@
 module.exports = ({ env }) => ({
-    upload: {
-      provider: 'cloudinary',
+  upload: {
+    config: {
+      provider: "cloudinary",
       providerOptions: {
-        cloud_name: env(process.env.CLOUD_NAME),
-        api_key: env(process.env.CLOUD_API_KEY),
-        api_secret: env(process.env.CLOUD_SECRET),
+        cloud_name: env("CLOUD_NAME"),
+        api_key: env("CLOUD_API_KEY"),
+        api_secret: env("CLOUD_SECRET"),
       },
-    },    
-  });
+    },
+  },
+});
