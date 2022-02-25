18828b9fde5024c575679843f2125958b8fc483a
Bleddyn Pijpers <bleddyn.pijpers@cloud-studio.co.uk>
2022-02-25 23:38:46 +0800

    added individual pages

---
+++ src/api/business/controllers/business.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ *  business controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::business.business', () => ({
+    async find(ctx) {
+        const populateList = [
+            'header',
+            'header.header_image',
+            'header.buttons',
+            'product_intro',
+            'product_intro.image',
+            'accordions',
+            'trajecten',
+            'trajecten.image',
+            'trajecten.buttons',
+            'search_engines',
+            'search_engines.og_image'
+        ]
+        // Push any additional query params to the array
+        populateList.push(ctx.query.populate)
+        ctx.query.populate = populateList.join(',')
+
+        const content = await super.find(ctx)
+        return content
+    }
+}));
