d10633081aea5d498e6dae204a4100de5769de8a
Bleddyn Pijpers <bleddyn.pijpers@cloud-studio.co.uk>
2022-03-31 14:27:53 +0800

    find by slug

--- src/api/article/controllers/article.js
+++ src/api/article/controllers/article.js
@@ -24,4 +24,29 @@ module.exports = createCoreController("api::article.article", ({ strapi }) => ({
     const content = await super.find(ctx);
     return content;
   },
+
+  async findOne(ctx) {
+
+    const populateList = [
+        "header",
+        "header.header_image",
+        "author",
+        "author.author_image",
+        "body",
+        "search_engines",
+        "search_engines.og_image",
+      ];
+      // Push any additional query params to the array
+      populateList.push(ctx.query.populate);
+      ctx.query.populate = populateList.join(",");
+
+    const { id: slug } = ctx.params;
+    const { query } = ctx;
+    if (!query.filters) query.filters = {}
+    query.filters.slug = { '$eq': slug }
+    const entity = await strapi.service('api::article.article').find(query);
+    const { results } = await this.sanitizeOutput(entity, ctx);
+    return this.transformResponse(results[0]);
+  }
+
 }));
